b86ad050e4484c392c58894213d2a521c7dac40c
Hoang Duy <hoangduy@mobcec.com>
2025-11-17 09:48:11 +0700

    remove animation

--- apps/animation/app/unfold/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UnfoldPage() {
-  return <div>UnfoldPage</div>;
-}
--- apps/animation/config/route.config.ts
+++
@@ -1,4 +0,0 @@
-export const MAIN_ROUTE = "/animation";
-export const LIST_ROUTE = {
-  UNFOLD: `/unfold`,
-};
